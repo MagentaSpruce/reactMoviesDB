3e3ac7324194217d51a97d722d6dd5dbe4dcdd1b
MagentaSpruce <81999424+MagentaSpruce@users.noreply.github.com>
2021-07-25 19:06:57 -0400

    Updates

---
+++ precache-manifest.88951efc16031dd932b75b979a609fc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d015bd9f7b666603df670d534303db11",
+    "url": "/reactMoviesDB/index.html"
+  },
+  {
+    "revision": "1bbe477687040523a37e",
+    "url": "/reactMoviesDB/static/css/main.3d22e596.chunk.css"
+  },
+  {
+    "revision": "290aa4f4295c4e36967e",
+    "url": "/reactMoviesDB/static/js/2.33260d0b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactMoviesDB/static/js/2.33260d0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bbe477687040523a37e",
+    "url": "/reactMoviesDB/static/js/main.7087622a.chunk.js"
+  },
+  {
+    "revision": "a1dbf44a1c30a3d16e0f",
+    "url": "/reactMoviesDB/static/js/runtime-main.9dd6f5ea.js"
+  }
+]);
